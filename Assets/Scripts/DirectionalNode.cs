2ec15c1bb4d3c1cb3b0505424c53d702c5cf63e1
Sera Ward <163619100+ssenwosgithub@users.noreply.github.com>
2024-04-06 20:34:50 -0400

    fix

---
+++ Assets/Scripts/DirectionalNode.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalNode : MonoBehaviour
+{
+    public Sprite none;
+    public Sprite left;
+    public Sprite right;
+    public string current;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        SwitchSprite();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SwitchSprite()
+    {
+        if(current == "left")
+        {
+            sr.sprite = left;
+        }
+        else if(current == "right")
+        {
+            sr.sprite = right;
+        }
+        else
+        {
+            sr.sprite = none;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Logic Circles</h1>
+
+Updated: added player!
